806f2fceac5e5cf62aa39800a67f5297597796fa
LessiaSa <0162221@mail.ru>
2024-06-03 12:17:46 +0700

    Formatted

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
     public int calculate(int price) {
         int result;
         int moneySpent = 20;
-        result = price/ moneySpent;
+        result = price / moneySpent;
         return result;
     }
 }
